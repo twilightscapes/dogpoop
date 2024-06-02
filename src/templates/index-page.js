12fcf743f4b164d38f7a6dc2b1aa9dfa6ef792f9
Todd <ihatespam@gmail.com>
2024-06-01 22:06:03 -0500

    logo

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -676,7 +676,34 @@ style={{height:'auto', width:'100vw', maxHeight:'', position:'relative', zIndex:
 )}
 {/* end show feature */}
 
+{/* <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="99GULJ7R9T68W" />
+  <table>
+    <tr>
+      <td>
+        <input type="hidden" name="on0" value="Available Plans:"/>
+        Available Plans:
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <select name="os0">
+          <option value="Weekly Service">
+            Weekly Service $29.99 USD
+          </option>
+          <option value="2X Weekly Service">
+            2X Weekly Service $49.99 USD
+          </option>
+        </select>
+      </td>
+    </tr>
+  </table>
+  <input type="hidden" name="currency_code" value="USD" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+</form> */}
 
+{/* <a href="https://www.paypal.com/webapps/billing/plans/subscribe?plan_id=P-7YL811853N7360003MZNWQKA" target="_blank" className="print" style={{color:'', fontSize:'', margin:'5px auto', textAlign:'center', textDecoration:'underline', maxWidth:'600px', padding:'0 2rem'}}>Order This</a> */}
 
 
 {/* show Info */}
